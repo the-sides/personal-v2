94df6ccace88aed77ed8b2a9e7e1ffe24739190b
the-sides <jacob.r.sides@gmail.com>
2024-06-15 01:01:25 -0400

    linting is mean again

--- src/lib/gsap/index.ts
+++ src/lib/gsap/index.ts
@@ -1,8 +1,6 @@
 import gsap from 'gsap'
 import TextPlugin from 'gsap/TextPlugin'
 
-let GSDevTools
-
 const GOLDEN_RATIO = (1 + Math.sqrt(5)) / 2
 const RECIPROCAL_GR = 1 / GOLDEN_RATIO
 const DURATION = Number(RECIPROCAL_GR.toFixed(3))
